9c796df7dae0e3b703e046b776d9ed3b4654ea63
drrh12 <flavioandrade0201@gmail.com>
2021-12-10 16:48:07 +0100

    Dashboard&Marketplace components added

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./dashboard.scss";
+
+function Dashboard() {
+  return <div></div>;
+}
+
+export default Dashboard;
---
+++ src/components/Marketplace/Marketplace.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./marketplace.scss";
+
+function Marketplace() {
+  return <div></div>;
+}
+
+export default Marketplace;
