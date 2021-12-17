406a8e7d48c3f122bf2c6711e7b48066383e72e0
drrh12 <flavioandrade0201@gmail.com>
2021-12-17 13:15:40 +0000

    Adjusted menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import "./App.css";
+
 import SideMenu, { menuItems } from "./components/Menu/SideMenu";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const Dashboard = () => <h1>Dashboard</h1>;
-const Content = () => <h1>Content</h1>;
-const Courses = () => <h1>Content/Courses</h1>;
-const Videos = () => <h1>Content/Videos</h1>;
-const Design = () => <h1>Design</h1>;
-const Content2 = () => <h1>Content2</h1>;
-const Courses2 = () => <h1>Content/Courses 2</h1>;
-const Videos2 = () => <h1>Content/Videos 2</h1>;
-const Design2 = () => <h1>Design 2</h1>;
+import { useState } from "react";
+import MarketPlace from "./components/MarketPlace/MarketPlace";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   const [inactive, setInactive] = useState(false);
@@ -26,25 +20,20 @@ function App() {
             setInactive(inactive);
           }}
         />
-
-        <div className={`container ${inactive ? "inactive" : ""}`}>
-          {/* Looping through menuItems instead of hard coding all the routes */}
-
-          {menuItems.map((menu, index) => (
-            <>
-              <Route key={menu.name} exact={menu.exact} path={menu.to}>
-                <h1>{menu.name}</h1>
-              </Route>
-              {menu.subMenus && menu.subMenus.length > 0
-                ? menu.subMenus.map((subMenu, i) => (
-                    <Route key={subMenu.name} path={subMenu.to}>
-                      <h1>{subMenu.name}</h1>
-                    </Route>
-                  ))
-                : null}
-            </>
-          ))}
-        </div>
+        <Routes>
+          <Route path="/marketplace" element={<MarketPlace />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Routes>
+        {/* <div className={`container ${inactive ? "inactive" : ""}`}>
+          <Switch>
+            <Route path="/Marketplace">
+              <MarketPlace></MarketPlace>
+            </Route>
+            <Route path="/Dashboard">
+              <Dashboard></Dashboard>
+            </Route>
+          </Switch>
+        </div> */}
       </Router>
     </div>
   );
